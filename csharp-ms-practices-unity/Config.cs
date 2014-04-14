d2b5cb4dd004af43e02165d8c00842464055b1c8
James Gosnell <jamesgosnell@gmail.com>
2014-04-14 09:01:01 -0500

    ADDED app.config

---
+++ csharp-ms-practices-unity/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+
+namespace csharp_ms_practices_unity
+{
+    public class Config
+    {
+        IUnityContainer Container = new UnityContainer().LoadConfiguration("someObject");
+    }
+}
